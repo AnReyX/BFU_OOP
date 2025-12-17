c8658aa383456107170c87273a25c34c4c3a0a97
AndrewG <151661194+AnReyX@users.noreply.github.com>
2025-12-17 22:12:13 +0200

    Delete Lab5 directory

--- Lab5/IUserRepository.py
+++
@@ -1,6 +0,0 @@
-from IDataRepository import IDataRepository
-from User import User
-
-class IUserRepository(IDataRepository[User]):
-    def get_by_login(self, login: str) -> User | None:
-        ...
